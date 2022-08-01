ef9f6d27f1110a856e8e85fd8cb6016e562b9723
nnaakkaaii <ultraganbayu10@gmail.com>
2022-08-01 23:53:58 +0900

    apiのエンドポイントを変更

--- web/src/utils/constant.ts
+++ web/src/utils/constant.ts
@@ -1,2 +1,3 @@
-export const API_URL = 'https://d2l1gd9w1ynhyi.cloudfront.net/api'
+// export const API_URL = 'https://d2l1gd9w1ynhyi.cloudfront.net/api'
+export const API_URL = 'https://shogi.y-nakai.com/api'
 // export const API_URL = 'http://localhost:8000/move'
